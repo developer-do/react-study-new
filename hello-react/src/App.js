18c955a1de2ed1e203bb6971688213a512c819f0
developer-do <dev-do@naver.com>
2020-07-14 21:58:57 +0900

    class 대신 className

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -3,22 +3,7 @@ import './App.css';
 
 function App() {
   const name = '리액트';
-  // const style = {
-  //   // background-color는 backgroundColor와 같이 -가 사라지고 카멜 표기법으로 작성됩니다.
-  //   backgroundColor: 'black',
-  //   color: 'aqua',
-  //   fontSize: '48px',
-  //   fontWeight: 'bold',
-  //   padding: 16
-  // };
-  return <div style={{
-    // background-color는 backgroundColor와 같이 -가 사라지고 카멜 표기법으로 작성됩니다.
-    backgroundColor: 'black',
-    color: 'aqua',
-    fontSize: '48px',
-    fontWeight: 'bold',
-    padding: 16
-  }}>{name}</div>
+  return <div className='react'>{name}</div>
 }
 
 export default App;
