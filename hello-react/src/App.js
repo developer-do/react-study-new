167aead0ff94e90b339b3d65a738adbb26f6b56f
developer-do <dev-do@naver.com>
2020-07-14 23:10:40 +0900

    class형 component일 때 state 사용법

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import './App.css';
 import MyComponent from './MyComponent';
+import Counter from './Counter';
 
 const App = () => {
-  return <MyComponent name="Dohyun" favoriteNumber={213}>리액트</MyComponent>
+  return <Counter />
 }
 
 export default App;
