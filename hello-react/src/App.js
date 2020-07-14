9efddeeccc19bb642acc8fdaa0b0882fb360fd4a
developer-do <dev-do@naver.com>
2020-07-14 21:32:33 +0900

    undefined를 렌더링 하지 않기

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -2,18 +2,9 @@ import React, { Fragment } from 'react';
 import './App.css';
 
 function App() {
-  const name = '리액트';
-  return (
-    <>
-      {name === '리액트' ? (
-        <h1>리액트입니다.</h1>  
-      ) : (
-        <h2>리액트가 아닙니다.</h2>
-      )};
-      
-      <div>{ name === '리액트' && <h1>리액트 입니다.</h1>}</div>
-    </>
-  );
+  const name = undefined;
+  // return name || '값이 undefined 입니다.';
+  return <div>{name}</div>;
 }
 
 export default App;
