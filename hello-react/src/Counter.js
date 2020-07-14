ce4f8ad91e3fc0fc2c7324db3445ce9aaae985c3
developer-do <dev-do@naver.com>
2020-07-14 23:25:21 +0900

    this.setState가 끝난 후 특정 작업 실행하기

--- hello-react/src/Counter.js
+++ hello-react/src/Counter.js
@@ -1,14 +1,10 @@
 import React, { Component } from 'react';
 
 class Counter extends Component {
-  constructor(props) {
-    super(props);
-    // state의 초깃값 설정하기
-    this.state = {
-      number: 0,
-      fixedNumber: 0
-    };
-  }
+  state = {
+    number: 0,
+    fixedNumber: 0
+  };
   render() {
     const { number, fixedNumber } = this.state; // state를 조회할 때는 this.state로 조회합니다.
     return (
@@ -21,7 +17,31 @@ class Counter extends Component {
             // this.setState를 사용하여 state에 새로운 값을 넣을 수 있습니다.
             this.setState({
               number: number + 1,
-              // fixedNumber: fixedNumber + 2,
+              // fixedNumber: this.state.fixedNumber + 2,
+            });
+
+            // this.setState를 두번 쓴다고 해서 두번 더해지는 게 아닙니다.
+            // this.setState({
+            //   number: this.state.number + 1
+            // });
+
+            this.setState( prevState => {
+              return {
+                number: prevState.number + 1
+              }
+            },
+            () => {
+              console.log("방금 setState가 호출되었습니다.");
+              console.log(this.state);
+            });
+            this.setState( prevState => {
+              return {
+                number: prevState.number + 1
+              }
+            },
+            () => {
+              console.log("방금 setState가 호출되었습니다.");
+              console.log(this.state);
             });
           }}
         >
