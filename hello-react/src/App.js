2c9be05149b4a8f84c466fc685c658fc0f3992b0
developer-do <dev-do@naver.com>
2020-07-15 00:08:45 +0900

    한 컴포넌트에서 userState 여러 번 사용해보기

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -2,9 +2,10 @@ import React from 'react';
 import './App.css';
 import MyComponent from './MyComponent';
 import Counter from './Counter';
+import Say from './Say';
 
 const App = () => {
-  return <Counter />
+  return <Say />
 }
 
 export default App;
