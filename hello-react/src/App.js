7cf21fe46d66901b41990b68b70d254002df5bfd
developer-do <dev-do@naver.com>
2020-07-14 21:56:34 +0900

    인라인 스타일링

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -2,9 +2,23 @@ import React, { Fragment } from 'react';
 import './App.css';
 
 function App() {
-  const name = undefined;
-  // return name || '값이 undefined 입니다.';
-  return <div>{name}</div>;
+  const name = '리액트';
+  // const style = {
+  //   // background-color는 backgroundColor와 같이 -가 사라지고 카멜 표기법으로 작성됩니다.
+  //   backgroundColor: 'black',
+  //   color: 'aqua',
+  //   fontSize: '48px',
+  //   fontWeight: 'bold',
+  //   padding: 16
+  // };
+  return <div style={{
+    // background-color는 backgroundColor와 같이 -가 사라지고 카멜 표기법으로 작성됩니다.
+    backgroundColor: 'black',
+    color: 'aqua',
+    fontSize: '48px',
+    fontWeight: 'bold',
+    padding: 16
+  }}>{name}</div>
 }
 
 export default App;
