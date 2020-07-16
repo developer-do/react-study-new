1973bd46f63128116e869fd7fd91d72d0e802454
developer-do <dev-do@naver.com>
2020-07-16 22:01:10 +0900

    state에 input value값 담기

--- hello-react/src/EventPractice.js
+++ hello-react/src/EventPractice.js
@@ -1,17 +1,28 @@
 import React, { Component } from 'react';
 
 class EventPractice extends Component {
+  
+  state = {
+    message: ''
+  }
+
   render() {
+    const { message } = this.state;
     return (
+      
       <div>
         <h1>이벤트 연습</h1>
         <input 
           type="text"
           name="message"
           placeholder="아무거나 입력해보세요."
+          value={message}
           onChange={
             (e) => {
-              console.log(e.target.value);
+              this.setState({
+                message: e.target.value
+              });
+              console.log(message);
             }
           }
         />
