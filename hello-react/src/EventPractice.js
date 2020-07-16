eec09dc751c5e2bceebe8e315b8af9c3340a6bf8
developer-do <dev-do@naver.com>
2020-07-16 23:19:02 +0900

    함수형 컴포넌트로 구현해보기 1

--- hello-react/src/EventPractice.js
+++ hello-react/src/EventPractice.js
@@ -1,58 +1,47 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 
-class EventPractice extends Component {
-  
-  state = {
-    message: '',
-    username: '',
+const EventPractice = () => {
+  const [username, setUsername] = useState('');
+  const [message, setMessage] = useState('');
+  const onChangeUsername = (e) => {
+    setUsername(e.target.value);
+    console.log(username);
   }
-
-  handleChange = (e) => {
-    this.setState({
-      [e.target.name]: e.target.value
-    });
+  const onChagenMessage = (e) => {
+    setMessage(e.target.value);
+    console.log(message);
   }
-
-  handleClick = () => {
-    alert(this.state.username + " : " + this.state.message);
-    this.setState({
-      message: '',
-      username: '',
-    });
+  const onClick = () => {
+    alert(username + ': ' + message);
+    setUsername('');
+    setMessage('');
   }
-
-  handleKeyPress = (e) => {
+  const onKeyPress = e => {
     if(e.key === "Enter") {
-      this.handleClick();
+      onClick();
     }
   }
-
-  render() {
-    const { message, username } = this.state;
-    return (
-      <div>
-        <h1>이벤트 연습</h1>
-        <input 
-          type="text"
-          name="message"
-          placeholder="아무거나 입력해보세요."
-          value={message}
-          onChange={this.handleChange}
-        />
-        <input 
-          type="text"
-          name="username"
-          placeholder="이름을 입력하세요."
-          value={username}
-          onChange={this.handleChange}
-          onKeyPress={this.handleKeyPress}
-        />
-        <button onClick={this.handleClick}>
-          확인
-        </button>
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      <h1>이벤트 연습</h1>
+      <input 
+        type="text"
+        name="username"
+        placeholder="사용자명"
+        value={username}
+        onChange={onChangeUsername}
+      />
+      <input 
+        type="text"
+        name="message"
+        placeholder="아무거나 입력해 보세요"
+        value={message}
+        onChange={onChagenMessage}
+        onKeyPress={onKeyPress}
+      />
+      <button onClick={onClick}>확인</button>
+    </div>
+  );
+};
 
 export default EventPractice;
