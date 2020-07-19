a4bf3db1f10a4dd5a2fa7dfc25cc973e2719f3d3
developer-do <dev-do@naver.com>
2020-07-20 01:12:00 +0900

    데이터 배열을 컴포넌트 배열로 변환해보기. 컴포넌트 수정 단계

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -7,25 +7,26 @@ import EventPractice from './EventPractice';
 import ValidationSample from './ValidationSample';
 import Refsample from './Refsample';
 import ScrollBox from './ScrollBox';
+import IterationSample from './IterationSample';
 
-class App extends Component {
-  render () {
-    return (
-      <div>
-        <ScrollBox ref={(box) => this.scrollBox = box} />
-        <button onClick={() => this.scrollBox.scrollToBottom()}>
-          맨 밑으로
-        </button>
-        <button onClick={() => this.scrollBox.scrollToTop()}>
-          맨 위로
-        </button>
-      </div>
-    )
-  }
-}
-
-// const App = () => {
-  
+// class App extends Component {
+//   render () {
+//     return (
+//       <div>
+//         <ScrollBox ref={(box) => this.scrollBox = box} />
+//         <button onClick={() => this.scrollBox.scrollToBottom()}>
+//           맨 밑으로
+//         </button>
+//         <button onClick={() => this.scrollBox.scrollToTop()}>
+//           맨 위로
+//         </button>
+//       </div>
+//     )
+//   }
 // }
+
+const App = () => {
+  return <IterationSample />
+}
 
 export default App;
