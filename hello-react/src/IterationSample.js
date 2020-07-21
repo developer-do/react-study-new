ab8f9a8eef37c47a7ec9fb641a3a70521ddac001
developer-do <dev-do@naver.com>
2020-07-21 23:35:57 +0900

    map 사용 응용 초기상태 설정하기

--- hello-react/src/IterationSample.js
+++ hello-react/src/IterationSample.js
@@ -1,14 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const IterationSample = (props) => {
-  const names = ['눈사람', '얼음', '눈', '바람'];
-  const nameList = names.map((name, index) => <li key={index}>{name}</li>);
-  return (
-    <div>
-      <h2>{props.name}님의 리스트</h2>
-      <ul>{nameList}</ul>
-    </div>
-  );
+  const [names, setNames] = useState([
+    { id: 1, text: '눈사람' },
+    { id: 2, text: '얼음' },
+    { id: 3, text: '눈' },
+    { id: 4, text: '바람' }
+  ]);
+  const [inputText, setInputText] = useState('');
+  const [nextId, setNextId] = useState(names.length + 1); // 새로운 항목을 추가할 대 사용할 id
+  const namesList = names.map(names => <li key={names.id}>{names.text}</li>);
+
+  return <ul>{namesList}</ul>;
 };
 
 export default IterationSample;
