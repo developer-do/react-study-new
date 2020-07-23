69b4d3618ed32354bae723234fa813d1681b798a
developer-do <dev-do@naver.com>
2020-07-24 00:41:43 +0900

    useState 특정 값이 업데이트 될 때만 실행하고 싶을 때

--- hooks-tutorial/src/Info.js
+++ hooks-tutorial/src/Info.js
@@ -3,14 +3,25 @@ import React, { useState, useEffect } from 'react';
 const Info = () => {
   const [name, setName] = useState('');
   const [nickname, setNickName] = useState('');
-
+  
+  /**
+   * useEffect를 사용할 때 특정 값이 변경될 때만 호출하고 싶을 경우에 클래스형 컴포넌트에서의 사용법
+   * componentDidUpdate(prevProps, prevState) {
+   *    if(prevProps.value !== this.props.value) {
+   *        doSomething();
+   *    }
+   * }
+   * 
+   * 함수형 컴포넌트에서는 useEffect의 두번째 파라미터 배열에 업데이트되면 호출하고싶은 특정 값을 배열에 추가
+   */
   useEffect(() => {
     console.log('렌더링이 완료되었습니다.');
     console.log({
       name,
       nickname
     });
-  }, []);
+  }, [name]);
+
 
   const onChangeName = e => {
     setName(e.target.value);
