21d4abad85c1cbc605460de6b6b0b1fb54e57550
developer-do <dev-do@naver.com>
2020-07-23 23:25:41 +0900

    App 컴포넌트에서 예제 컴포넌트 사용

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -10,6 +10,31 @@ import ScrollBox from './ScrollBox';
 import IterationSample from './IterationSample';
 import LifeCycleSample from './LifeCycleSample';
 
+function getRandomColor() {
+  return '#' + Math.floor(Math.random() * 16777215).toString(16);
+}
+
+class App extends React.Component {
+  state = {
+    color: '#000000'
+  }
+
+  handleClick = () => {
+    this.setState({
+      color: getRandomColor()
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.handleClick}>랜덤 색상</button>
+        <LifeCycleSample color={this.state.color} />
+      </div>
+    )
+  }
+}
+
 // class App extends Component {
 //   render () {
 //     return (
@@ -26,8 +51,8 @@ import LifeCycleSample from './LifeCycleSample';
 //   }
 // }
 
-const App = () => {
-  return <LifeCycleSample />
-}
+// const App = () => {
+//   return <LifeCycleSample />
+// }
 
 export default App;
