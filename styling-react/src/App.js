ca01786cc1e09fdef627b59af45f06ab02e1fa94
dev-do <dev-do@naver.com>
2020-07-26 12:36:44 +0900

    styled-components 사용해보기

--- styling-react/src/App.js
+++ styling-react/src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import SassComponent from './SassComponent';
 import CSSModule from './CSSModule';
+import StyledComponent from './StyledComponent';
 
 function App() {
   return (
     <div>
       {/* <SassComponent /> */}
-      <CSSModule />
+      <StyledComponent />
     </div>
   );
 }
