072df651320b54b6b528230982e6aed0b1fa8dcb
dev-do <dev-do@naver.com>
2020-07-27 01:34:55 +0900

    TodoTemplate 만들기

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,10 +1,9 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import TodoTemplate from "./components/TodoTemplate";
 
 function App() {
-  return <div>
-    Todo App을 만들자!  
-  </div>
+  return <TodoTemplate>Todo App을 만들자!</TodoTemplate>;
 }
 
 export default App;
