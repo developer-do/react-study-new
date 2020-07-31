cf8c6fc2c72f5cf963800f87db03d60bcbad63bd
dev-do <dev-do@naver.com>
2020-07-31 21:48:57 +0900

    todolist 삭제기능 추가

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,4 +1,4 @@
-import React, {useState, useCallback, useRef } from "react";
+import React, { useState, useCallback, useRef } from "react";
 import "./App.css";
 import TodoTemplate from "./components/TodoTemplate";
 import TodoInsert from "./components/TodoInsert";
@@ -8,17 +8,17 @@ function App() {
   const [todos, setTodos] = useState([
     {
       id: 1,
-      text: '리액트 기초 알아보기',
+      text: "리액트 기초 알아보기",
       checked: true,
     },
     {
       id: 2,
-      text: '컴포넌트 스타일링 해보기',
+      text: "컴포넌트 스타일링 해보기",
       checked: true,
     },
     {
       id: 3,
-      text: '일정 관리 앱 만들어 보기',
+      text: "일정 관리 앱 만들어 보기",
       checked: false,
     },
   ]);
@@ -26,20 +26,29 @@ function App() {
   // 고유값으로 사용될 id
   // ref를 사용하여 변수 담기
   const nextId = useRef(todos.length + 1);
-  const onInsert = useCallback( (text) => { 
-    const todo = {
-      id: nextId.current,
-      text,
-      checked: false,
+  const onInsert = useCallback(
+    (text) => {
+      const todo = {
+        id: nextId.current,
+        text,
+        checked: false,
+      };
+      setTodos(todos.concat(todo));
+      nextId.current += 1;
+    },
+    [todos]
+  );
+
+  const onRemove = useCallback(
+    id => {
+      setTodos(todos.filter(todo => todo.id !== id));
     }
-    setTodos(todos.concat(todo));
-    nextId.current += 1;
-  }, [todos]);
+  );
 
   return (
     <TodoTemplate>
       <TodoInsert onInsert={onInsert} />
-      <TodoList todos={todos} />
+      <TodoList todos={todos} onRemove={onRemove} />
     </TodoTemplate>
   );
 }
