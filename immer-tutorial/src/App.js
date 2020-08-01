34c112ed5aec6e3796986f4acfd0eb2ecfeb14de
dev-do <dev-do@naver.com>
2020-08-01 21:27:59 +0900

    App 컴포넌트에 immer 적용해보기

--- immer-tutorial/src/App.js
+++ immer-tutorial/src/App.js
@@ -1,27 +1,25 @@
-import React, { useRef, useCallback, useState } from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React, { useRef, useState, useCallback } from "react";
+import produce from "immer";
 
-function App() {
+const App = () => {
   const nextId = useRef(1);
   const [form, setForm] = useState({ name: "", username: "" });
-  const [data, setData] = useState({
-    array: [],
-    uselessValue: null,
-  });
+  const [data, setData] = useState({ array: [], uselessValue: null });
 
   // input 수정을 위한 함수
   const onChange = useCallback(
     (e) => {
       const { name, value } = e.target;
-      setForm({
-        ...form,
-        [name]: [value],
-      });
+      setForm(
+        produce(form, draft => {
+          draft[name] = value;
+        })
+      );
     },
     [form]
   );
 
+  // form 등록을 위한 함수
   const onSubmit = useCallback(
     (e) => {
       e.preventDefault();
@@ -32,15 +30,16 @@ function App() {
       };
 
       // array에 새 항목 등록
-      setData({
-        ...data,
-        array: data.array.concat(info),
-      });
+      setData(
+        produce(data, (draft) => {
+          draft.array.push(info);
+        })
+      );
 
       // form 초기화
       setForm({
         name: "",
-        username: "",
+        value: "",
       });
       nextId.current += 1;
     },
@@ -50,25 +49,28 @@ function App() {
   // 항목을 삭제하는 함수
   const onRemove = useCallback(
     (id) => {
-      setData({
-        ...data,
-        array: data.array.filter((info) => info.id !== id),
-      });
+      setData(
+        produce(data, (draft) => {
+          draft.array.splice(
+            draft.array.findIndex((info) => info.id === id),
+            1
+          );
+        })
+      );
     },
     [data]
   );
 
-
   return (
     <div>
       <form onSubmit={onSubmit}>
-        <input 
+        <input
           name="username"
           placeholder="아이디"
           value={form.username}
           onChange={onChange}
         />
-        <input 
+        <input
           name="name"
           placeholder="이름"
           value={form.name}
@@ -78,7 +80,7 @@ function App() {
       </form>
       <div>
         <ul>
-          {data.array.map( info => (
+          {data.array.map((info) => (
             <li key={info.id} onClick={() => onRemove(info.id)}>
               {info.username} ({info.name})
             </li>
@@ -86,7 +88,7 @@ function App() {
         </ul>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default App;
