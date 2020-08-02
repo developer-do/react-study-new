30df6826cf9c1f98ff97ae0f7924e1b8265c4761
dev-do <dev-do@naver.com>
2020-08-02 10:41:50 +0900

    URL 파라미터

--- router-tutorial/src/App.js
+++ router-tutorial/src/App.js
@@ -1,7 +1,8 @@
-import React from 'react';
-import { Route, Link } from 'react-router-dom';
-import Home from './component/Home';
-import About from './component/About';
+import React from "react";
+import { Route, Link } from "react-router-dom";
+import Home from "./component/Home";
+import About from "./component/About";
+import Profile from "./component/Profile";
 
 function App() {
   return (
@@ -13,9 +14,16 @@ function App() {
         <li>
           <Link to="/About">소개</Link>
         </li>
+        <li>
+          <Link to="/profile/velopert">velopert의 프로필</Link>
+        </li>
+        <li>
+          <Link to="/profile/dohyun">dohyun의 프로필</Link>
+        </li>
       </ul>
       <Route path="/" component={Home} exact={true} />
-      <Route path={['/about', '/info']} component={About} />
+      <Route path={["/about", "/info"]} component={About} />
+      <Route path="/profile/:username" component={Profile} />
     </div>
   );
 }
---
+++ router-tutorial/src/component/Profile.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+const data = {
+  velopert: {
+    name: "김민준",
+    description: "리액트를 좋아하는 개발자",
+  },
+  dohyun: {
+    name: "김도현",
+    description: "열심히 하지만 잘 안되는 개발자",
+  },
+};
+
+const Profile = ({ match }) => {
+  const { username } = match.params;
+  const profile = data[username];
+  if(!profile) {
+    return <div>존재하지 않는 사용자 입니다.</div>;
+  }
+  return (
+    <div>
+      <h3>
+        {username}({Profile.name})
+      </h3>
+      <p>{profile.description}</p>
+    </div>
+  );
+};
+
+export default Profile;
