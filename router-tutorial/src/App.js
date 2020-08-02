02dd5d4d0eb8979c0f7f7da0d2760ccb053e80cd
dev-do <dev-do@naver.com>
2020-08-02 11:39:51 +0900

    리액트 라우터 부가기능 history 사용

--- router-tutorial/src/App.js
+++ router-tutorial/src/App.js
@@ -4,6 +4,7 @@ import Home from "./component/Home";
 import About from "./component/About";
 import Profile from "./component/Profile";
 import Profiles from "./component/Profiles";
+import HistorySample from "./component/HistorySample";
 
 function App() {
   return (
@@ -18,11 +19,15 @@ function App() {
         <li>
           <Link to="/profiles">프로필</Link>
         </li>
+        <li>
+          <Link to="/history">history 예제</Link>
+        </li>
       </ul>
       <Route path="/" component={Home} exact={true} />
       <Route path={["/about", "/info"]} component={About} />
       <Route path="/profile/:username" component={Profile} />
-      <Route path="/Profiles" component={Profiles} />
+      <Route path="/profiles" component={Profiles} />
+      <Route path="/history" component={HistorySample} />
     </div>
   );
 }
