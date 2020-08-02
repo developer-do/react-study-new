d1649d66cbee66dac5e6c95dd2ee662adac8da43
dev-do <dev-do@naver.com>
2020-08-02 11:21:43 +0900

    서브라우트 사용해보기

--- router-tutorial/src/App.js
+++ router-tutorial/src/App.js
@@ -3,6 +3,7 @@ import { Route, Link } from "react-router-dom";
 import Home from "./component/Home";
 import About from "./component/About";
 import Profile from "./component/Profile";
+import Profiles from "./component/Profiles";
 
 function App() {
   return (
@@ -15,15 +16,13 @@ function App() {
           <Link to="/about">소개</Link>
         </li>
         <li>
-          <Link to="/profile/velopert">velopert의 프로필</Link>
-        </li>
-        <li>
-          <Link to="/profile/dohyun">dohyun의 프로필</Link>
+          <Link to="/profiles">프로필</Link>
         </li>
       </ul>
       <Route path="/" component={Home} exact={true} />
       <Route path={["/about", "/info"]} component={About} />
       <Route path="/profile/:username" component={Profile} />
+      <Route path="/Profiles" component={Profiles} />
     </div>
   );
 }
