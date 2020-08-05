cc2a8a0634994d38ee316d8a3072c9bd3f62c253
dev-do <dev-do@naver.com>
2020-08-05 19:06:53 +0900

    뉴스 뷰어 UI 만들기

--- news-viewer/src/components/NewsList.js
+++ news-viewer/src/components/NewsList.js
@@ -1,55 +1,40 @@
 import React from 'react';
 import styled from 'styled-components';
+import NewsItem from './NewsItem';
 
-const NewsItemBlock = styled.div`
-  display: flex;
-  .thumbnail {
-    margin-right: 1rem;
-  }
-  img {
-    display: block;
-    width: 160px;
-    height: 100px;
-    object-fit: cover;
-  }
-  .contents {
-    h2 {
-      margin: 0;
-      a {
-        color: black;
-      }
-    }
-    p {
-      margin: 0;
-      line-height: 1.5;
-      margin-top: 0.5rem;
-      white-space: normal;
-    }
-  }
-  & + & {
-    margin-top: 3rem;
+const NewsListBlock = styled.div`
+  box-sizing: border-box;
+  padding-bottom: 3rem;
+  width: 768px;
+  margin: 0 auto;
+  margin-top: 2rem;
+  @media screen and (max-width: 768px) {
+    width: 100%;
+    padding-left: 1rem;
+    padding-right: 1rem;
   }
 `;
 
-const NewsList = ({ article }) => {
+const sampleArticle = {
+  title: '제목',
+  description: '내용',
+  url: 'https://google.com',
+  urlToImage: 'https://via.placeholder.com/160',
+};
+
+console.log(sampleArticle);
+
+const NewsList = () => {
   return (
-    <NewsItemBlock>
-      {urlToImage && (
-        <div className="thumbnail">
-          <a href="{url}" target="_blank" rel="noopener noreferrer">
-            <img src="{urlToImage}" alt="{title}" />
-          </a>
-        </div>
-      )}
-      <div className="contents">
-        <h2>
-          <a href="{url}" target="_blank" rel="noopener noreferrer">
-            {title}
-          </a>
-        </h2>
-        <p>{description}</p>
-      </div>
-    </NewsItemBlock>
+    <NewsListBlock>
+      <NewsItem article={sampleArticle} />
+      <NewsItem article={sampleArticle} />
+      <NewsItem article={sampleArticle} />
+      <NewsItem article={sampleArticle} />
+      <NewsItem article={sampleArticle} />
+      <NewsItem article={sampleArticle} />
+      <NewsItem article={sampleArticle} />
+    </NewsListBlock>
   );
 };
 
