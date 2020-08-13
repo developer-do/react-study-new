9ffd096808a08cf95301d43e41b3363c72b6f5aa
dev-do <dev-do@naver.com>
2020-08-14 01:14:24 +0900

    context api Provider 사용해보기

--- context-tutorial/src/App.js
+++ context-tutorial/src/App.js
@@ -2,12 +2,15 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import ColorBox from "./components/ColorBox";
+import ColorContext from "./contexts/color";
 
 function App() {
   return (
-    <div className="App">
-      <ColorBox />
-    </div>
+    <ColorContext.Provider value={{ color: "red" }}>
+      <div className="App">
+        <ColorBox />
+      </div>
+    </ColorContext.Provider>
   );
 }
 
